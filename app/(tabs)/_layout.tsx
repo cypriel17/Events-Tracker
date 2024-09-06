d53d3a8fa8fced23b6f90b0289af41d236268e20
Z.Ngomane <zngomane023a@student.wethinkcode.co.za>
2024-09-06 21:36:15 +0200

    authentication flow and protecting tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,19 @@
-import { Link, Tabs } from 'expo-router';
+import { Link, Redirect, Tabs, useRouter } from 'expo-router';
 
 import { HeaderButton } from '../../components/HeaderButton';
 import { TabBarIcon } from '../../components/TabBarIcon';
+import { useAuth } from '~/contexts/AuthProvider';
 
 export default function TabLayout() {
+const { isAuthenticated } = useAuth();
+const router = useRouter();
+
+  if (!isAuthenticated){
+    console.warn("Not signed in!")
+    router.push('/login');  // Redirect to login
+    return null;
+  }
+  
   return (
     <Tabs
       screenOptions={{
