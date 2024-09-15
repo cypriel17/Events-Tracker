1169032b1d3dbe4bd196c0555ca1347b09afd53a
Z.Ngomane <zngomane023a@student.wethinkcode.co.za>
2024-09-15 02:35:41 +0200

    user avatar or profile picture upload option

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,66 +1,67 @@
 import { Stack } from 'expo-router';
 import { useEffect, useState } from 'react';
 import { Alert, Button, Pressable, TextInput, View, Text } from 'react-native';
+
+import Avatar from '~/components/Avatar';
 import { useAuth } from '~/contexts/AuthProvider';
 import { supabase } from '~/utils/supabase';
 
 export default function Profile() {
-
   const [loading, setLoading] = useState(true);
   const [username, setUsername] = useState('');
-  const [fullname, setFullname] = useState('');
   const [website, setWebsite] = useState('');
+  const [fullName, setFullName] = useState('');
+
   const [avatarUrl, setAvatarUrl] = useState('');
 
   const { session } = useAuth();
 
   useEffect(() => {
-    if (session) getProfile()
-  }, [session])
+    if (session) getProfile();
+  }, [session]);
 
   async function getProfile() {
     try {
-      setLoading(true)
-      if (!session?.user) throw new Error('No user on the session!')
+      setLoading(true);
+      if (!session?.user) throw new Error('No user on the session!');
 
       const { data, error, status } = await supabase
         .from('profiles')
         .select(`username, website, avatar_url, full_name`)
         .eq('id', session?.user.id)
-        .single()
+        .single();
       if (error && status !== 406) {
-        throw error
+        throw error;
       }
 
       if (data) {
-        setUsername(data.username)
-        setFullname(data.full_name)
-        setWebsite(data.website)
-        setAvatarUrl(data.avatar_url)
+        setUsername(data.username);
+        setWebsite(data.website);
+        setAvatarUrl(data.avatar_url);
+        setFullName(data.full_name);
       }
     } catch (error) {
       if (error instanceof Error) {
-        Alert.alert(error.message)
+        Alert.alert(error.message);
       }
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
-async function updateProfile({
+  async function updateProfile({
     username,
     website,
     avatar_url,
-    full_name
+    full_name,
   }: {
-    username: string
-    website: string
-    avatar_url: string
-    full_name: string
+    username: string;
+    website: string;
+    avatar_url: string;
   }) {
     try {
-      setLoading(true)
-      if (!session?.user) throw new Error('No user on the session!')
+      setLoading(true);
+      if (!session?.user) throw new Error('No user on the session!');
 
       const updates = {
         id: session?.user.id,
@@ -69,67 +70,79 @@ async function updateProfile({
         avatar_url,
         full_name,
         updated_at: new Date(),
-      }
+      };
 
-      const { error } = await supabase.from('profiles').upsert(updates)
+      const { error } = await supabase.from('profiles').upsert(updates);
 
       if (error) {
-        throw error
+        throw error;
       }
     } catch (error) {
       if (error instanceof Error) {
-        Alert.alert(error.message)
+        Alert.alert(error.message);
       }
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
   return (
-    <View className='flex-1 bg-white p-5 gap-3'>
+    <View className="flex-1 gap-3 bg-white p-5">
       <Stack.Screen options={{ title: 'Profile' }} />
 
-      <TextInput
-          editable={false}
-          value={session?.user.email}
-          placeholder="username"
-          autoCapitalize={'none'}
-          className="border rounded-md border-grey-200 p-3 text-black-600"
+      <View className="items-center ">
+        <Avatar
+          size={200}
+          url={avatarUrl}
+          onUpload={(url: string) => {
+            setAvatarUrl(url);
+            updateProfile({ username, website, avatar_url: url });
+          }}
         />
-
-      <TextInput
-          onChangeText={(text) => setFullname(text)}
-          value={fullname}
-          placeholder="full name"
-          autoCapitalize={'none'}
-          className="border rounded-md border-grey-200 p-3"
-        />
+      </View>
 
       <TextInput
-          onChangeText={(text) => setUsername(text)}
-          value={username}
-          placeholder="username"
-          autoCapitalize={'none'}
-          className="border rounded-md border-grey-200 p-3"
-        />
+        editable={false}
+        value={session!.user.email}
+        placeholder="email"
+        autoCapitalize="none"
+        className="rounded-md border border-gray-200 p-3 text-gray-600"
+      />
 
       <TextInput
-          onChangeText={(text) => setWebsite(text)}
-          value={website}
-          placeholder="website"
-          autoCapitalize={'none'}
-          className="border rounded-md border-grey-200 p-3"
-        />
+        onChangeText={(text) => setFullName(text)}
+        value={fullName}
+        placeholder="full name"
+        autoCapitalize="none"
+        className="rounded-md border border-gray-200 p-3"
+      />
 
-      <Pressable 
-            onPress={() => updateProfile({ username, website, avatar_url: avatarUrl, full_name: fullname })} 
-            disabled={loading} 
-            className="items-center rounded-md border-2 border-red-500 p-3 px-8">
-          <Text className="text-lg font-bold text-red">Update</Text>
-        </Pressable>
+      <TextInput
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+        placeholder="username"
+        autoCapitalize="none"
+        className="rounded-md border border-gray-200 p-3"
+      />
 
-      <Button title='Sign Out' onPress={() => supabase.auth.signOut()} />
+      <TextInput
+        onChangeText={(text) => setWebsite(text)}
+        value={website}
+        placeholder="website"
+        autoCapitalize="none"
+        className="rounded-md border border-gray-200 p-3"
+      />
+
+      <Pressable
+        onPress={() =>
+          updateProfile({ username, website, avatar_url: avatarUrl, full_name: fullName })
+        }
+        disabled={loading}
+        className="items-center rounded-md border-2 border-red-500 p-3 px-8">
+        <Text className="text-lg font-bold text-red-500">Save</Text>
+      </Pressable>
+
+      <Button title="Sign out" onPress={() => supabase.auth.signOut()} />
     </View>
   );
 }
-
