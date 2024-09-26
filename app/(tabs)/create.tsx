dd8c2ee47dd531343f996c0fd25737a2bee48ed9
Z.Ngomane <zngomane023a@student.wethinkcode.co.za>
2024-09-26 21:46:41 +0200

    filtering location and address autocomplete with mapbox api

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,102 +1,116 @@
-import { useState } from 'react';
-import { Alert, Button, Pressable, Text, TextInput, View } from 'react-native';
-import DatePicker from 'react-native-date-picker';
-import { supabase } from '~/utils/supabase';
-import { useAuth } from '~/contexts/AuthProvider';
 import { router } from 'expo-router';
+import { useState } from 'react';
+import { Text, View, TextInput, Button, Pressable, Alert, ScrollView } from 'react-native';
+import DatePicker from 'react-native-date-picker';
+
+import AddressAutocomplete from '~/components/AddressAutocomplete';
 import Avatar from '~/components/Avatar';
+import { useAuth } from '~/contexts/AuthProvider';
+import { supabase } from '~/utils/supabase';
 
-export default function CreateEvent(){
-    const [date, setDate] = useState(new Date());
-    const [open, setOpen] = useState(false);
+export default function CreateEvent() {
+  const [open, setOpen] = useState(false);
 
-    const { user } = useAuth()
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [imageUrl, setImageUrl] = useState('');
+  const [location, setLocation] = useState(null);
 
-    const [loading, setLoading] = useState(false);
-    const [title, setTitle] = useState('title');
-    const [description, setDescription] = useState('');
-    const [imageUrl, setImageUrl] = useState('');
+  const [loading, setLoading] = useState(false);
 
-    const createEvent = async () => {
-        setLoading(true);
-        const { data, error } = await supabase
-        .from('events')
-        .insert([
-            { 
-                title, 
-                description,
-                date: date.toISOString(),
-                user_id: user?.id,
-                image_uri: imageUrl,
-            },
-        ])
-        .select()
-        .single() // since data is an array, we just want one event
+  const { user } = useAuth();
 
-        if (error) {
-            Alert.alert('Error creating event', error.message)
-            return
-        } else {
-            setTitle('');
-            setDescription('');
-            setDate(new Date());
-            setLoading(false);
-            router.push(`/event/${data?.id}`)
-        }
-             
-    };
+  const createEvent = async () => {
+    setLoading(true);
 
-    return (
-        <View className='flex-1 bg-white p-5 gap-3'>
-            <View className="items-center ">
-                <Avatar
-                size={200}
-                url={imageUrl}
-                onUpload={(url: string) => {
-                    setImageUrl(url);
-                }}
-                />
-            </View>
-            <TextInput 
-                value={title}
-                // onChangeText={(text) => setTitle(text)}
-                onChangeText={setTitle}
-                placeholder='Title' 
-                className="border rounded-md border-grey-200 p-3" />
-            <TextInput 
-                value={description}
-                // onChangeText={(text) => setDescription(text)}
-                onChangeText={setDescription}
-                placeholder='Description' 
-                multiline={true}
-                numberOfLines={3}
-                className="min-h-42 border rounded-md border-grey-200 p-3" />
+    const long = location?.features[0].geometry.coordinates[0];
+    const lat = location?.features[0].geometry.coordinates[1];
 
-            <Text 
-                className="border rounded-md border-grey-200 p-3" 
-                onPress={() => setOpen(true)}>{date.toLocaleString()}</Text>
-            <DatePicker
-                    modal
-                    open={open}
-                    date={date}
-                    minimumDate={new Date()}
-                    minuteInterval={15}
-                    onConfirm={(date) => {
-                    setOpen(false)
-                    setDate(date)
-                    }}
-                    onCancel={() => {
-                    setOpen(false)
-                    }}
-                />
+    const { data, error } = await supabase
+      .from('events')
+      .insert([
+        {
+          title,
+          description,
+          date: date.toISOString(),
+          user_id: user?.id,
+          image_uri: imageUrl,
+          location: location?.features[0].properties.name,
+          location_point: `POINT(${long} ${lat})`,
+        },
+      ])
+      .select()
+      .single();
 
-            <Pressable 
-                disabled={loading} 
-                onPress={() => createEvent()} 
-                className="mt-auto items-center rounded-md bg-red-500 p-3 px-8">
-                <Text className="text-lg font-bold text-white">Create Event</Text>
-            </Pressable>
-        </View>
-    );
+    if (error) {
+      Alert.alert('Failed to create the event', error.message);
+    } else {
+      setTitle('');
+      setDescription('');
+      setDate(new Date());
+      console.log(data);
+      router.push(`/event/${data.id}`);
+    }
 
+    setLoading(false);
+  };
+
+  return (
+    <ScrollView className="flex-1" contentContainerClassName="gap-3 bg-white p-5">
+      <View className="items-center ">
+        <Avatar
+          size={200}
+          url={imageUrl}
+          onUpload={(url: string) => {
+            setImageUrl(url);
+          }}
+        />
+      </View>
+
+      <TextInput
+        value={title}
+        // onChangeText={(text) => setTitle(text)}
+        onChangeText={setTitle}
+        placeholder="Title"
+        className="rounded-md border border-gray-200 p-3"
+      />
+      <TextInput
+        value={description}
+        onChangeText={setDescription}
+        placeholder="Description"
+        multiline
+        numberOfLines={3}
+        className="min-h-32 rounded-md border border-gray-200 p-3"
+      />
+
+      <Text className="rounded-md border border-gray-200 p-3" onPress={() => setOpen(true)}>
+        {date.toLocaleString()}
+      </Text>
+
+      <DatePicker
+        modal
+        open={open}
+        date={date}
+        minimumDate={new Date()}
+        minuteInterval={15}
+        onConfirm={(date) => {
+          setOpen(false);
+          setDate(date);
+        }}
+        onCancel={() => {
+          setOpen(false);
+        }}
+      />
+
+      <AddressAutocomplete onSelected={(location) => setLocation(location)} />
+
+      <Pressable
+        onPress={() => createEvent()}
+        disabled={loading}
+        className="mt-auto items-center rounded-md bg-red-500 p-3 px-8">
+        <Text className="text-lg font-bold text-white">Create event</Text>
+      </Pressable>
+    </ScrollView>
+  );
 }
